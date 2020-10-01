1ef30e2422314ee6040be57b50475fd44790111c
Denis Snitko <denissnitko@gmail.com>
2020-10-01 12:03:01 +0300

    add s-all-set

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,26 @@
 
+let thumbs = new Swiper('.swiper-container-thumbs', {
+  slidesPerView: 5,
+  spaceBetween: 18,
+  watchSlidesVisibility: true,
+  initialSlide: true,
+  loop: true
+});
+
+let itemSwiper = document.querySelectorAll('.swiper-container-product');
+
+itemSwiper.forEach(swiper => {
+  new Swiper(swiper, {
+    slidesPerView: 1,
+
+    navigation: {
+      nextEl: swiper.querySelector('.swiper-button-next'),
+      prevEl: swiper.querySelector('.swiper-button-prev')
+    },
+
+    thumbs: {
+      // swiper: sMounterThumbs
+    }
+  })
+})
+
