483691df456a1615ecf7d49d538797b7495bf525
Denis Snitko <denissnitko@gmail.com>
2020-10-01 14:54:49 +0300

    Update

---
+++ #src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ironbox</title>
+</head>
+
+<body>
+  <h3>Список страниц</h3>
+  <ul style="list-style-type: none; font-size: 18px;">
+    <li><a href="index.html">index.html</a></li>
+  </ul>
+</body>
+
+</html>
