0459d804f67cd23dc9dd4fb42fa0259fd1ddc0c1
Denis Snitko <denissnitko@gmail.com>
2020-10-02 15:50:03 +0300

    Update s-reviews

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,17 +1,20 @@
 
-let thumbs = new Swiper('.swiper-container-thumbs', {
-  slidesPerView: 5,
-  spaceBetween: 18,
-  watchSlidesVisibility: true,
-  initialSlide: true,
-  loop: true
-});
 
 let itemSwiper = document.querySelectorAll('.swiper-container-product');
 
 itemSwiper.forEach(swiper => {
+
+  let thumbs = new Swiper(swiper.nextElementSibling, {
+    slidesPerView: 5,
+    spaceBetween: 18,
+    watchSlidesVisibility: true,
+    initialSlide: true,
+    loop: true
+  });
+
   new Swiper(swiper, {
     slidesPerView: 1,
+    loop: true,
 
     navigation: {
       nextEl: swiper.querySelector('.swiper-button-next'),
@@ -19,9 +22,10 @@ itemSwiper.forEach(swiper => {
     },
 
     thumbs: {
-      // swiper: sMounterThumbs
+      swiper: thumbs
     }
   })
+
 })
 
 // SWIPER REWIEWS
@@ -37,12 +41,35 @@ let swiper = new Swiper('.swiper-container-reviews', {
   },
 });
 
-// ACCORDEON
+
 
 window.addEventListener('click', (event) => {
+
+  // Accordeon
   if (event.target.hasAttribute('data-accordeon_trigger')) {
     event.target.classList.toggle('accordeon__item-title--active');
     event.target.nextElementSibling.classList.toggle('accordeon__item-text--show');
+  }
 
+  // Counter minus
+  if (event.target.hasAttribute('data-minus')) {
+    let counter = event.target.closest('.counter').querySelector('[data-counter]');
+    if (counter.value == 1) {
+      counter.value = 1;
+    } else {
+      counter.value = parseInt(counter.value) - 1;
+    }
+  }
+
+  // Counter plus
+  if (event.target.hasAttribute('data-plus')) {
+    let counter = event.target.closest('.counter').querySelector('[data-counter]');
+    counter.value = parseInt(counter.value) + 1;
+  }
+
+  // Show / Hide text in reviews
+  if (event.target.hasAttribute('data-review-btn')) {
+    event.target.previousElementSibling.classList.toggle('s-reviews__text--toggle');
   }
 })
+
