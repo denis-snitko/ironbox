ffe821e705bfc35feed939682a5cf19be00752c5
Denis Snitko <denissnitko@gmail.com>
2020-10-04 20:45:56 +0300

    update tinify

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,8 @@ let path = {
     html: [sourceFolder + "/*.html", "!" + sourceFolder + "/_*.html"],
     css: sourceFolder + "/scss/main.scss",
     js: sourceFolder + "/js/*.js",
-    img: sourceFolder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    img: sourceFolder + "/img/**/*.{jpg,png,gif,ico,webp}",
+    svg: sourceFolder + "/img/**/*.svg",
     fonts: sourceFolder + "/fonts/*.ttf"
   },
 
@@ -25,7 +26,8 @@ let path = {
     html: sourceFolder + "/**/*.html",
     css: sourceFolder + "/**/*.scss",
     js: sourceFolder + "/**/*.js",
-    img: sourceFolder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    img: sourceFolder + "/img/**/*.{jpg,png,gif,ico,webp}",
+    svg: sourceFolder + "/img/**/*.svg",
   },
 
   clean: "./" + projectFolder + "/"
@@ -43,7 +45,7 @@ let cleanCss = require('gulp-clean-css');
 let rename = require('gulp-rename');
 let uglify = require('gulp-uglify-es').default;
 let imagemin = require('gulp-imagemin');
-let imgCompress = require('imagemin-jpeg-recompress');
+let tinify = require('gulp-tinify');
 let ttf2woff = require('gulp-ttf2woff');
 let ttf2woff2 = require('gulp-ttf2woff2');
 
@@ -110,22 +112,20 @@ function js() {
 
 function img() {
   return src(path.src.img)
-    .pipe(
-      imagemin({
-        // progressive: true,
-        // interlaced: true,
-        optimizationLevel: 4 // 0 to 7
-      })
-    )
-    .pipe(imagemin([
-      imgCompress({
-        loops: 4,
-        min: 70,
-        max: 90,
-        quality: 'high'
-      }),
-      imagemin.svgo()
-    ]))
+    // .pipe(
+    //   imagemin({
+    //     // progressive: true,
+    //     // interlaced: true,
+    //     optimizationLevel: 4 // 0 to 7
+    //   })
+    // )
+    .pipe(tinify('GB8W9Gddw3tr0GRgMK5sX0rt7g2tVkQV'))
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
+
+function svg() {
+  return src(path.src.svg)
     .pipe(dest(path.build.img))
     .pipe(browsersync.stream())
 }
@@ -195,6 +195,7 @@ function watchFiles() {
   gulp.watch([path.watch.css], css);
   gulp.watch([path.watch.js], js);
   gulp.watch([path.watch.img], img);
+  gulp.watch([path.watch.svg], svg);
 }
 
 function fonts() {
@@ -210,13 +211,14 @@ function clean() {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, grid, gulp.parallel(fonts, html, css, js, img));
+let build = gulp.series(clean, grid, gulp.parallel(fonts, html, css, js, svg, img));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 exports.html = html;
 exports.css = css;
 exports.js = js;
 exports.img = img;
+exports.svg = svg;
 exports.fonts = fonts;
 exports.fontsStyle = fontsStyle;
 exports.build = build;
