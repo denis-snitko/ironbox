f9a0d19d8465b1049f7df5b3dd1b30d2afecf7db
Denis Snitko <denissnitko@gmail.com>
2020-10-06 16:51:02 +0300

    Update adaptive

--- #src/index.php
+++ #src/index.php
@@ -13,11 +13,11 @@
     <li><a href="index.html">Главная</a></li>
     <li><a href="catalog-page.html">Каталог товаров</a></li>
     <li><a href="item-page.html">Карточка твара</a></li>
-    <li><a href="about-page.html">О нас</a></li>
-    <li><a href="delivery-page.html">Доставка и оплата</a></li>
-    <li><a href="reviews-page.html">Отзывы о товарах</a></li>
-    <li><a href="contacts-page.html">Контакты</a></li>
-    <!-- <li><a href="cart-page.html">Корзина</a></li> -->
+    <li><a href="about-page.html">О нас</a></li> <!-- + адаптив 80% -->
+    <li><a href="delivery-page.html">Доставка и оплата</a></li> <!-- + адаптив -->
+    <li><a href="reviews-page.html">Отзывы о товарах</a></li> <!-- + адаптив -->
+    <li><a href="contacts-page.html">Контакты</a></li> <!-- + адаптив -->
+    <li><a href="cart-page.html">Корзина</a></li>
   </ul>
 </body>
 
